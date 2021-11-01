75fc2d03d15332785a26e8258ebfaf234e1e0ad0
stephylonappan <stephylonappan14@gmail.com>
2021-11-01 09:08:32 +0530

    modified gitlearning

--- src/dev/gitlearning.java
+++ src/dev/gitlearning.java
@@ -6,5 +6,8 @@ public class gitlearning {
 	LoginPage lp=new LoginPage();
     public static void main(String args[]) {
     	System.out.println("in Home page");
+    	int j=100;
+    	String test="commit check";
+    	System.out.println(test + " "+ j);
     }
 }
