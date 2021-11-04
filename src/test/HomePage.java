4a075b91779dea562cf785b3f0e52490249a961b
stephylonappan <stephylonappan14@gmail.com>
2021-11-04 22:48:15 +0530

    modified file as per review comment

--- src/test/HomePage.java
+++ src/test/HomePage.java
@@ -6,13 +6,14 @@ public class HomePage {
 	static String plan1="git";
 	static String plan2="graph automation";
 	static String plan3="Driving";
-	String plan4="java collections";
-	String plan5="oops concept";
+	static String plan4="java collections";
+	static String plan5="oops concept";
 	String plan6="Jenkins/joules";
 	String plan7="Api testing";
 	
 	public static void main(String args[]) {
 		System.out.println("home page :" +learning + "   "+plan1+"  "+plan2);
+		System.out.println("new changes:"+ plan3 +" "+ plan4 +" "+ plan5);
 	}
 	
 }
